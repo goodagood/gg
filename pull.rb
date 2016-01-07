68f1dba1baa2ee272a69234e345267fa36bf73fa
Andrew <bin1510@gmail.com>
2016-01-07 14:37:13 +0000

    "modify ruby pull, to delete big logging"

--- pull.rb
+++ pull.rb
@@ -2,7 +2,7 @@
 #
 
 project_dir   = "/home/ubuntu/workspace/gg"
-log_file_path = "/tmp/pulljob"
+$log_file_path = "/tmp/pulljob"
 
 branch_to_merge_from = "7188"
 
@@ -11,34 +11,49 @@ merge_cmd = "git merge origin/#{branch_to_merge_from}"
 
 reg = /\*.+#{branch_to_merge_from}/ 
 
-# do fetching from origin, after cd the project directory.
-fetched = %x<cd #{project_dir}; #{fetch_cmd}>
-puts fetched
-
-# check if we are not at the wrong branch 
-branch_cmd = "git branch -a"
-branches   = %x<#{branch_cmd}>
-puts branches
-
-if reg.match(branches)
-    puts 'matched, not suppose to merge self'
-    puts 'going to abort running'
-    abort()
-end
-
-
-puts 'not finished? going to do merge'
-merged  = %x<cd #{project_dir}; #{merge_cmd}>
-puts merged
 
 def append_log (msg)
-    File.open(log_file_path, 'w'){|f|
+    file_size = File.stat($log_file_path).size
+    if file_size > 32000
+        File.delete($log_file_path)
+    end
+    
+    File.open($log_file_path, 'a'){|f|
         f.write(Time.new)
         f.write("\r\n")
         f.write(msg)
         f.write("\r\n" * 2)
     }
 end
+
+
+# do fetching from origin, after cd the project directory.
+fetched = %x<cd #{project_dir}; #{fetch_cmd}>
+#puts fetched
+append_log(fetched)
+
+# check if we are not at the wrong branch 
+branch_cmd = "git branch -a"
+branches   = %x<#{branch_cmd}>
+#puts branches
+append_log(branches)
+
+if reg.match(branches)
+    #puts 'matched, not suppose to merge self'
+    append_log('matched, not suppose to merge self')
+    #puts 'going to abort running'
+    append_log('going to abort running')
+
+    abort()
+end
+
+
+#puts 'not finished? going to do merge'
+append_log('not finished? going to do merge')
+merged  = %x<cd #{project_dir}; #{merge_cmd}>
+#puts merged
+append_log(merged)
+
 
 # to set this run every 10 minutes:
 # crontab -u username -e
